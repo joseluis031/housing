2c3b837f85055b4a62a8247a99eb93c39a6cf928
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-05-08 11:12:15 +0200

    he conseguido clustering con outlider de precio

--- Clases/ejercicio.py
+++ Clases/ejercicio.py
@@ -191,9 +191,50 @@ warnings.filterwarnings('ignore')
 class Clustering:
     def __init__(self,datos):
         self.datos = pd.read_csv(datos)
-    
+        self.datos.pop("Address")
     def cluster(self):
+        # Escalado de datos
+        # ==============================================================================
+        X = self.datos.drop(columns='Price').to_numpy()
+        X_scaled = scale(X)
         
+        # Modelo
+        # ==============================================================================
+        modelo_dbscan = DBSCAN(
+                            eps          = 0.2,
+                            min_samples  = 5,
+                            metric       = 'euclidean',
+                        )
+
+        modelo_dbscan.fit(X=X_scaled)
+
+        # Clasificación
+        # ==============================================================================
+        labels = modelo_dbscan.labels_
+
+        fig, ax = plt.subplots(1, 1, figsize=(4.5, 4.5))
+
+        ax.scatter(
+            x = X[:, 0],
+            y = X[:, 1], 
+            c = labels,
+            marker    = 'o',
+            edgecolor = 'black'
+        )
+
+        # Los outliers se identifican con el label -1
+        ax.scatter(
+            x = X[labels == -1, 0],
+            y = X[labels == -1, 1], 
+            c = 'red',
+            marker    = 'o',
+            edgecolor = 'black',
+            label = 'outliers'
+        )
+
+        ax.legend()
+        ax.set_title('Clusterings generados por DBSCAN');
+        plt.show()
         
 hola5 = Clustering("USA_Housing.csv")
 print(hola5.cluster())
